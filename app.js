3972dd5835b68e69da7861315156d16c8559b1f8
whitepanguin <dun04045@gmail.com>
2025-04-22 17:43:34 +0900

    add static

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import fs from "fs";
 
 const app = express();
 const box = {};
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  fs.readFile("/index.html", (err, data) => {
+  fs.readFile(__dirname + "/views/index.html", (err, data) => {
     if (err) {
       res.status(500);
       return res.send("파일 읽기 오류");
